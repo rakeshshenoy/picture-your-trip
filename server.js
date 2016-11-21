a04e30d51d9867c57fdea7277063ba347fd57c5e
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2016-11-21 01:23:17 -0800

    First commit

--- server.js
+++ server.js
@@ -3,22 +3,17 @@ var http = require("http")
 var parser=require("xml2js").parseString;
 var request = require('request');
 var Clarifai = require('clarifai');
-Clarifai.initialize({'clientId':'NYtWYvzRYwh9gnXni64n60y6z9n5cH34PEdnm3uS', 'clientSecret':'yiHV8xG4MoL-d251yYv_C7caeGYrU_-NAFYLQrp9'});
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
 var async=require('async');
 var InstagramStrategy = require('passport-instagram').Strategy;
-var INSTAGRAM_CLIENT_ID ="82fc03a1f11f4a33bfd720b10f966582";
-var INSTAGRAM_CLIENT_SECRET ="c2b1c9ee461f446bad5199a82464802e";
 var probs = [];
 var classes = [];
 var tags = [];
 var places=[];
 var city;
 var finalres;
-removeArr = ['People', 'Television', 'Entrance', 'Logo', 'Details', 'Summer', 'Stairs', 'Winter','Spring','Autumn', 'Restaurant', 'Wedding', 'Daytime', 'Food & Beverages', 'Reception', 'Terrace', 'Wardrobe', 'Bed', 'Kids Area', 'Breakfast Buffet', 'Couch', 'Living Room', 'Building', 'Nighttime', 'Fireplace', 'Gift Shop', 'Coffee Machine', 'Ballroom', 'Internet Area', 'Telephone', 'Bathroom Amenities', 'Lamp', 'Fountain', 'Bar'];
 typeFilter = ['accounting', 'airport', 'atm', 'bakery', 'bank', 'bar', 'beauty_salon', 'bicycle_store', 'book_store', 'bus_station', 'car_dealer', 'car_rental', 'car_repair', 'car_wash', 'cemetery', 'city_hall', 'clothing_store', 'convenience_store', 'courthouse', 'dentist', 'department_store', 'doctor', 'electrician', 'electronics_store', 'embassy', 'finance', 'fire_station', 'florist', 'food', 'funeral_home', 'furniture_store', 'gas_station', 'general_contractor', 'grocery_or_supermarket', 'gym', 'hair_care', 'hardware_store', 'health', 'home_goods_store', 'hospital', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'library', 'liquor_store', 'local_government_office', 'locksmith', 'meal_delivery', 'meal_takeaway', 'movie_rental', 'movie_theater', 'moving_company', 'painter', 'parking', 'pet_store', 'pharmacy', 'physiotherapist', 'plumber', 'police', 'post_office', 'real_estate_agency', 'restaurant', 'roofing_contractor', 'school', 'shoe_store', 'storage', 'store', 'subway_station', 'taxi_stand', 'train_station', 'transit_station', 'travel_agency', 'university', 'veterinary_care', 'church', 'place_of_worship', 'mosque', 'premise', 'temple', 'cafe'];
-map = {'Garden':['Garden','Park','National Park'], 'Snow & Ski Sports':['Snow Ski'], 'Animals':['Zoo'], 'Building':['Monument'], 'Surroundings':['Overlook'], 'View':['Overlook']};
 
 function getPlace(tag, place)
 {
@@ -36,7 +31,7 @@ function getPlace(tag, place)
             //console.log(place.photo);
             if(place.photo != undefined)
             {
-                jObj['photo'] = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + place.photo[0].photo_reference + '&key=AIzaSyA6sfGYa21RdwF8L2muePF-naQMCFNpFMk';
+                jObj['photo'] = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + place.photo[0].photo_reference + '&key=' + key;
             }
             else
             {
@@ -72,8 +67,8 @@ function getTags(item, index)
 }
 
 passport.use(new Strategy({
-    clientID: "1190452254320209",
-    clientSecret: "8115ce3033f3072f3be4cd7ea38fbaf4",
+    clientID: FACEBOOK_CLIENT_ID,
+    clientSecret: FACEBOOK_CLIENT_SECRET,
     callbackURL: 'http://localhost:3000/login/facebook/return'
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -87,25 +82,11 @@ passport.use(new InstagramStrategy({
     callbackURL: "http://localhost:3000/auth/instagram/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
     tkn=accessToken;
-      // To keep the example simple, the user's Instagram profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the Instagram account with a user record in your database,
-      // and return that user instead.
       return done(null, profile);
    
   }
 ));
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
@@ -126,7 +107,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-//app.use(bodyp.json());
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
 // Initialize Passport and restore authentication state, if any, from the
@@ -189,7 +169,7 @@ app.post('/profiles',function (req, res,next){
             console.log(tags);
             places = [];
             async.forEach(tags, function(tag, callback){
-                var googleLink = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=" + encodeURIComponent(tag.toLowerCase() + " in " + city) + "&key=AIzaSyCRnt2j53o4RnwdN2pwXBIdGz4Ksd054tM"
+                var googleLink = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=" + encodeURIComponent(tag.toLowerCase() + " in " + city) + "&key=" + key
                 request(googleLink, function(error, response, body) {
                 if(!error && response.statusCode == 200){
                     parser(body, function (err, result) { 
